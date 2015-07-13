c45729ea1d2cf1518e99d4b09bd44013ec3a28bb
TAG <arthur.gwatidzo@gmail.com>
2015-07-13 12:25:12 +0200

    first commit:files from harsh 13April2015

---
+++ scripts/contextChanges.js
@@ -0,0 +1,5 @@
+/**
+ * Created by hisp on 9/7/15.
+ */
+
+var dhis2BaseUrl = "../../../";
---
+++ scripts/report.js
@@ -0,0 +1,6 @@
+/**
+ * Created by hisp on 9/7/15.
+ */
+var generateReport = function(){
+    window.location.href = "reports/Dev_Sample_I.html?pe=2015&ou=JLA7wl59oN3";
+}
